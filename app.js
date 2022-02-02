86f390fa48a0c2c40386ac6e24a8316809c9cee2
nodrex <nodo.imedashvili@gmail.comm>
2022-02-02 23:58:14 +0400

    remove unnecesarry packages

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 require('./database');
-// require('./database/redis');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const session = require('./util/session');
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session);
 app.use('/api', routes);
 
 app.listen(3000);
